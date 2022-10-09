f98dcd53c8b864966ff2240af7f2596644da6dc8
CalvinTai0402 <calvintai0402@gmail.com>
2022-10-09 14:26:03 -0700

    test

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -94,7 +94,7 @@ export default function AdminBase() {
   //   return () => {
   //     abortControllerRefCurrent.abort();
   //     clearInterval(intervalRef.current);
-  //   };
+  //   }; //test
   // }, [currentUserId]);
 
   if (loading) {
