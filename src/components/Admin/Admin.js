6841b5341638bebe7b50b0429a62e89b0858d577
CalvinTai0402 <calvintai0402@gmail.com>
2022-10-09 12:19:06 -0700

    workaround for sharing cookies

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -16,42 +16,108 @@ import Dashboard from './Dashboard';
 export default function AdminBase() {
   const [loading, setLoading] = useState(true);
   const abortControllerRef = useRef(new AbortController());
+  const intervalDoubleDomainRef = useRef(null);
+  const [currentUserId, setCurrentUserId] = useState(null);
+  const [resDataId, setResDataId] = useState(null);
 
-  const dataProvider = DataProvider(process.env.REACT_APP_API_DATA_URL);
-  const MyUserMenu = (props) => (
-    <UserMenu {...props}>
-      <MyLogoutButton />
-    </UserMenu>
-  );
-  const MyAppBar = (props) => <AppBar {...props} userMenu={<MyUserMenu />} />;
-  const MyLayout = (props) => <Layout {...props} appBar={MyAppBar} />;
+  const reactAdminSignOut = async () => {
+    await signOut();
+    setCurrentUserId(null); // after signing out, we need to reset the currentUserId to null
+    setResDataId(null); // after signing out, we need to reset the resDataId to null
+    window.location.reload();
+  };
 
+  // Workaround for two sites sharing cookie
   useEffect(() => {
     const abortControllerRefCurrent = abortControllerRef.current;
-    const wrapper = async () => {
-      let res = await getCurrentUser(abortControllerRefCurrent);
-      if (res?.status !== 200 || res?.data.role !== 'admin') {
-        await signOut();
-        window.location.reload();
+    const helper = async () => {
+      if (!intervalDoubleDomainRef.current) {
+        intervalDoubleDomainRef.current = setInterval(async () => {
+          let res = await getCurrentUser(abortControllerRefCurrent);
+          if (res?.status === 200 && res?.data.role !== 'admin') {
+            await reactAdminSignOut();
+          } else if (res?.status === 200) {
+            setResDataId(res?.data.id);
+            setLoading(false);
+          }
+        }, 3000);
       }
-      setLoading(false);
     };
-    wrapper();
+    helper();
     return () => {
       abortControllerRefCurrent.abort();
+      clearInterval(intervalDoubleDomainRef.current);
     };
   }, []);
+
+  useEffect(() => {
+    const helper = async () => {
+      if (!currentUserId) {
+        setCurrentUserId(resDataId);
+      } else if (currentUserId !== resDataId) {
+        await reactAdminSignOut();
+      }
+    };
+    helper();
+  }, [currentUserId, resDataId]);
+  // End workaround for two sites sharing cookie
+
+  // useEffect(() => {
+  //   const abortControllerRefCurrent = abortControllerRef.current;
+  //   const wrapper = async () => {
+  //     let res = await getCurrentUser(abortControllerRefCurrent);
+  //     if (res?.status !== 200 || res?.data.role !== 'admin') {
+  //       await reactAdminSignOut();
+  //     }
+  //     setLoading(false);
+  //     setCurrentUserId(res?.data.id);
+  //   };
+  //   wrapper();
+  //   return () => {
+  //     abortControllerRefCurrent.abort();
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   if (!currentUserId) return;
+  //   const abortControllerRefCurrent = abortControllerRef.current;
+  //   if (!intervalRef.current) {
+  //     intervalRef.current = setInterval(async () => {
+  //       if (await doesSessionExist()) {
+  //         const res = await getCurrentUser(abortControllerRefCurrent);
+  //         if (currentUserId !== res?.data.id) {
+  //           await reactAdminSignOut();
+  //         }
+  //       }
+  //     }, 3000);
+  //   }
+  //   return () => {
+  //     abortControllerRefCurrent.abort();
+  //     clearInterval(intervalRef.current);
+  //   };
+  // }, [currentUserId]);
 
   if (loading) {
     return (
       <div className="flex h-screen">
         <div className="m-auto">
-          Loading
-          <Spinner size={10} />
+          <SessionAuth>
+            Loading
+            <Spinner size={10} />
+          </SessionAuth>
         </div>
       </div>
     );
   }
+
+  const dataProvider = DataProvider(process.env.REACT_APP_API_DATA_URL);
+  const MyUserMenu = (props) => (
+    <UserMenu {...props}>
+      <MyLogoutButton reactAdminSignOut={reactAdminSignOut} />
+    </UserMenu>
+  );
+  const MyAppBar = (props) => <AppBar {...props} userMenu={<MyUserMenu />} />;
+  const MyLayout = (props) => <Layout {...props} appBar={MyAppBar} />;
 
   return (
     <SessionAuth>
